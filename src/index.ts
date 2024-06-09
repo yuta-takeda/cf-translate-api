311ed308a8e85d6e6606b82b276801ac79b339c5
yuta-takeda <ytakeda@jmdc.co.jp>
2024-06-09 18:22:56 +0900

    create artists endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+type Bindings = {
+  TRANSLATION_DB: TRANSLATION_DB
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get("/api/v1/artists", async (c) => {
+  try {
+    const { results } = await c.env.TRANSLATION_DB.prepare("SELECT * FROM artists").all()
+    return c.json(results)
+  } catch (e) {
+    console.log(e)
+    return c.json({ err: e }, 500)
+  }
 })
 
 export default app
