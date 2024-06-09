ae29777138088e85448c216601e1cf4d9fc75b73
yuta-takeda <ytakeda@jmdc.co.jp>
2024-06-09 20:24:27 +0900

    create get endpoint

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,42 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>()
 
-app.get("/api/v1/artists", async (c) => {
+app.get("/api/v1/artist/:name", async (c) => {
   try {
-    const { results } = await c.env.TRANSLATION_DB.prepare("SELECT * FROM artists").all()
-    return c.json(results)
+    const name = await c.req.param("name")
+    const { results } = await c.env.TRANSLATION_DB.prepare("SELECT * FROM artists WHERE english_name = ? LIMIT ?")
+                               .bind(name, 1)
+                               .all()
+    const output = { name: results[0]?.japanese_name }
+    return c.json(output)
+  } catch (e) {
+    console.log(e)
+    return c.json({ err: e }, 500)
+  }
+})
+
+app.get("/api/v1/group/:name", async (c) => {
+  try {
+    const name = await c.req.param("name")
+    const { results } = await c.env.TRANSLATION_DB.prepare("SELECT * FROM groups WHERE english_name = ? LIMIT ?")
+                               .bind(name, 1)
+                               .all()
+    const output = { name: results[0]?.japanese_name }
+    return c.json(output)
+  } catch (e) {
+    console.log(e)
+    return c.json({ err: e }, 500)
+  }
+})
+
+app.get("/api/v1/series/:name", async (c) => {
+  try {
+    const name = await c.req.param("name")
+    const { results } = await c.env.TRANSLATION_DB.prepare("SELECT * FROM series WHERE english_name = ? LIMIT ?")
+                               .bind(name, 1)
+                               .all()
+    const output = { name: results[0]?.japanese_name }
+    return c.json(output)
   } catch (e) {
     console.log(e)
     return c.json({ err: e }, 500)
